6004a636892f8063f230dcbc028d28ff666a6704
allanpreis <allanpreis31@gmail.com>
2020-04-07 20:40:45 -0300

    update

--- MENU_OPCOES.py
+++ MENU_OPCOES.py
@@ -15,15 +15,15 @@ while opcao !=3:
         n2 = float(input('Segundo número: '))
         n3 = n1 + n2
         print('\n{} + {} = {}'.format(n1, n2, n3))
-
+        break
     elif opcao == 2:
         n1 = int(input('\nInsira um número: '))
         print('A raiz quadrada de {} é {}'.format(n1 , n1**2))
-
+        break
     elif opcao == 3:
         print('\nSaindo do programa...')
 
     else:
         print('\nVocê escolheu uma opção inválida!!!')
-print('Fim do programa')
+print('\nFim do programa')
 
--- NOTA_FINAL.py
+++ NOTA_FINAL.py
@@ -9,18 +9,16 @@ n3 = float(input('Exame Final: '))
 p1 = ((n1*2)+(n2*3)+(n3*5))
 p2 = 2+3+5
 resultado = p1 / p2
-if(resultado):
-    if (resultado >= 8.0 or resultado <= 10.0):
-        print('\nConceito A - 8.0 a 10.0')
-    elif (resultado >= 7.0 or resultado <= 7.9):
-            print('\nConceito B - 7.0 a 7.9')
-    elif (resultado >= 6.0 or resultado <= 6.9):
-            print('\nConeito C - 6.0 a 6.9')
-    elif (resultado >= 5.0 or resultado <= 5.9):
-            print('\nConceito D - 5.0 a 5.9')
+if (resultado >= 8.0 and resultado <= 10.0):
+    print('\nConceito A - 8.0 a 10.0')
+elif (resultado >= 7.0 and resultado <= 7.9):
+    print('\nConceito B - 7.0 a 7.9')
+elif (resultado >= 6.0 and resultado <= 6.9):
+    print('\nConceito C - 6.0 a 6.9')
+elif (resultado >= 5.0 and resultado <= 5.9):
+    print('\nConceito D - 5.0 a 5.9')
 else:
-    if (resultado >= 0.0 or resultado <=4.9):
-         print('\nConceito E - 0.0 a 4.9')
+    print('\nConceito E - 0.0 a 4.9')
 
 
 print('\nSua nota será {:.1f}'.format(resultado))
