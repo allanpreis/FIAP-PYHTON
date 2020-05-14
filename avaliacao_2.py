bd6153dbb3940dc59f04a5c390153cf47a90a483
allanpreis <allanpreis31@gmail.com>
2020-05-13 21:43:41 -0300

    update

--- avaliacao_2.py
+++ avaliacao_2.py
@@ -1,5 +1,4 @@
 print("Bem-vindo a empresa FOGUETE, escolha abaixo a opção que deseja para aquisição das passagens")
-
 janela = [0]*24
 corredor = [0]*24
 opcao = 0
@@ -12,49 +11,55 @@ while opcao !=5:
     print('-' * 35)
     opcao = int(input('Qual é a sua opção? '))
     if opcao == 1:
-        print('-'*20)
-        print('''    [1] Janela
+        if janela == [1]*24 and corredor == [1]*24:
+            print("\033[31mÔnibus lotado. Opção inválida!\033[m")
+        else:
+            print('-'*20)
+            print('''    [1] Janela
     [2] Corredor''')
-        print('-' * 20)
-        fileira = int(input("Qual fileira deseja? "))
-        if fileira == 1:
-            poltrona = int(input("Qual o número da poltrona deseja? "))
-            poltrona = (poltrona - 1)
-            if janela[poltrona] == 1:
-                print("\033[31mPoltrona ocupada. Venda não realizada!\033[m")
-            else:
-                janela[poltrona] = 1
-                print("\033[32mVenda realizada com sucesso!\033[m")
-        elif fileira == 2:
-            poltrona = int(input("Qual o número da poltrona deseja? "))
-            poltrona = (poltrona - 1)
-            if corredor[poltrona] == 1:
-                print("\033[31mPoltrona ocupada. Venda não realizada!\033[m")
-            else:
-                corredor[poltrona] = 1
-                print("\033[32mVenda realizada com sucesso!\033[m")
+            print('-' * 20)
+            fileira = int(input("Qual fileira deseja? "))
+            if fileira == 1:
+                poltrona = int(input("Qual o número da poltrona deseja? "))
+                poltrona = (poltrona - 1)
+                if janela[poltrona] == 1:
+                    print("\033[31mPoltrona ocupada. Venda não realizada!\033[m")
+                else:
+                    janela[poltrona] = 1
+                    print("\033[32mVenda realizada com sucesso!\033[m")
+            elif fileira == 2:
+                poltrona = int(input("Qual o número da poltrona deseja? "))
+                poltrona = (poltrona - 1)
+                if corredor[poltrona] == 1:
+                    print("\033[31mPoltrona ocupada. Venda não realizada!\033[m")
+                else:
+                    corredor[poltrona] = 1
+                    print("\033[32mVenda realizada com sucesso!\033[m")
     elif opcao == 2:
-        print('-' * 20)
-        print('''    [1] Janela
+        if janela == [0]*24 and corredor == [0]*24:
+            print("\033[31mTodas as poltronas estão livres. Opção inválida!\033[m")
+        else:
+            print('-' * 20)
+            print('''    [1] Janela
     [2] Corredor''')
-        print('-' * 20)
-        fileira = int(input("Qual fileira deseja? "))
-        if fileira == 1:
-            poltrona = int(input("Qual o número da poltrona deseja? "))
-            poltrona = (poltrona - 1)
-            if janela[poltrona] == 0:
-                print("\033[31mPoltrona livre. Compra não cancelada!\033[m")
-            else:
-                janela[poltrona] = 0
-                print("\033[32mCompra cancelada com sucesso! \033[m")
-        elif fileira == 2:
-            poltrona = int(input("Qual o número da poltrona deseja? "))
-            poltrona = (poltrona - 1)
-            if corredor[poltrona] == 0:
-                print("\033[31mPoltrona livre. Compra não cancelada!\033[m")
-            else:
-                corredor[poltrona] = 0
-                print("\033[32mCompra cancelada com sucesso! \033[m")
+            print('-' * 20)
+            fileira = int(input("Qual fileira deseja? "))
+            if fileira == 1:
+                poltrona = int(input("Qual o número da poltrona deseja? "))
+                poltrona = (poltrona - 1)
+                if janela[poltrona] == 0:
+                    print("\033[31mPoltrona livre. Compra não cancelada!\033[m")
+                else:
+                    janela[poltrona] = 0
+                    print("\033[32mCompra cancelada com sucesso! \033[m")
+            elif fileira == 2:
+                poltrona = int(input("Qual o número da poltrona deseja? "))
+                poltrona = (poltrona - 1)
+                if corredor[poltrona] == 0:
+                    print("\033[31mPoltrona livre. Compra não cancelada!\033[m")
+                else:
+                    corredor[poltrona] = 0
+                    print("\033[32mCompra cancelada com sucesso! \033[m")
 
     elif opcao == 3:
         janela_ocupada =  janela
